fe017d44a3b976d2627e33a1da0dc81affa2439b
JackieBinBin <772818851@qq.com>
2020-01-27 12:33:37 +0800

    fin_290_01实现-修正描述

--- fin_290_01.py
+++ fin_290_01.py
@@ -1,11 +1,12 @@
-x=99;y=77;z=88
+x=99;y=77;z=101
 
 zzh = [x, y, z]
 
 
 # enumerate(zzh) 是根据zzh 这个list返回一个键值序列对
 # 像这样 [(0, x),(1,y),(2,z)]，我后续操作都是对索引来的(0,1,2)，
-# 目的就是为了在排序后能追踪到原始数据
+# 目的就是为了在排序后能追踪到原始数据，
+# 相当于我没动原始数据zzh,只是找出了所有奇数并排序，
 # =======================
 
 # 这是个列表推导式，i for i in enumerate(zzh) if i[1]%2
@@ -16,7 +17,7 @@ sorted_odd_nums = sorted((i for i in enumerate(zzh) if i[1]%2),
                          key = lambda i:i[1],
                          reverse=True)
 
-print(sorted_odd_nums)
+print(sorted_odd_nums) # 这个为奇数列表
 
 
 if not sorted_odd_nums:
