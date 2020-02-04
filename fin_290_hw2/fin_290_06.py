cd8c5c7bf93545a35edd8519d55bac0d76bdda42
JackieBinBin <772818851@qq.com>
2020-02-04 17:02:23 +0800

    更新fin_290_6的实现和描述

---
+++ fin_290_hw2/fin_290_06.py
@@ -0,0 +1,36 @@
+import random
+
+# 题目出的太宽了，我限制了下就猜100内整数
+
+zzh = random.randint(1,100)
+
+guess_num = 0
+# 总共5次尝试机会
+attempts = 5
+
+print(f'Please guess the number in 1-100 interval~~')
+
+# 这里相当于是个死循环，不断的尝试，限制次数使用完成
+while int(guess_num) !=zzh and attempts !=0:
+
+    print(f'hey bro,input guess number: ')
+    print(f'Now u have {attempts} chances to try')
+    guess_num = input()
+
+    if not guess_num.isdigit(): # 判断输入的字符串是否为数字
+        print(f'Please input a number,not any other type of things: ')
+        guess_num = 0
+        attempts -= 1
+        continue
+    else:
+        if int(guess_num) > zzh:
+            print('oops,Bigger yo~~')
+        elif int(guess_num) < zzh:
+            print('oops,Smaller yo~~')
+    attempts -= 1
+
+if int(guess_num) == zzh:
+    print(f'Bingo!!!!!!!!!!!!!!!!NiuBlity')
+else:
+    print(f'GoodBye.No chances')
+    print(f'The number is :{zzh}' )
