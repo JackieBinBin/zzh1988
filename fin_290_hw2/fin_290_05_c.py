6fe1c34c4ea3f5a18f71a9b27dff407f43a517db
JackieBinBin <772818851@qq.com>
2020-02-04 16:02:23 +0800

    更新fin_290_5_c的实现和描述

--- fin_290_hw2/fin_290_05_c.py
+++ fin_290_hw2/fin_290_05_c.py
@@ -1,2 +1,15 @@
 # Write code that sorts a list of names into alphabetical order
 
+zzh_list = ['paul','tom','terra','Maxwell','Rosemary','Shirley']
+
+# sort函数 默认是by  alphabetical order
+
+zzh_list.sort()
+
+print(zzh_list)
+
+# 根据长度
+
+zzh_list.sort(key=len)
+
+print(zzh_list)
