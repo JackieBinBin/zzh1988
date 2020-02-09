497b20b1d90b018b3f5b1cf66fe036bf0a4e1c49
JackieBinBin <772818851@qq.com>
2020-02-09 22:48:34 +0800

    提交Fin 290 Practice Midterm 2题实现非def方法

--- fin_290_practice_midterm/fin_290_02.py
+++ fin_290_practice_midterm/fin_290_02.py
@@ -7,14 +7,31 @@ user_input = int(input(f'Please input a number N: '))
 # 核心即为每个数均为前两项之和
 # 用户输入的数字即为数据项长度
 
-def zzh_fibo (n):
-    if n == 1 or n == 2:
-        return 1
-    return zzh_fibo (n-1) + zzh_fibo (n-2)
+# def zzh_fibo (n):
+#     if n == 1 or n == 2:
+#         return 1
+#     return zzh_fibo (n-1) + zzh_fibo (n-2)
+#
+# fibo_list = []
+#
+# for i in range(1,user_input):
+#     fibo_list.append(zzh_fibo(i))
 
-fibo_list = []
+# print(f'fibo 序列结果：{fibo_list}')
 
-for i in range(1,user_input):
-    fibo_list.append(zzh_fibo(i))
+# 思路同上，N作为列表下标在不断的循环，
+#
 
-print(f'fibo 序列结果：{fibo_list}')
+i,num1,num2 = 0,1,1
+
+fibo_list = [] # 初始化fibo列表
+
+while i < user_input:
+    fibo_list.append(num1)
+
+    num1,num2 = num2,num1+num2
+
+    i += 1
+
+print(fibo_list)
+
